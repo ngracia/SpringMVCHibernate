d859b846833836cdd004ef493ba1e9b299d3f472
ngracia <nestor.gracia@gmail.com>
2016-05-25 23:45:58 +0100

    Getters and Mutators have been added to ResquestResponseDto.

--- src/main/java/com/ngracia/spring/dto/RequestResponseDto.java
+++ src/main/java/com/ngracia/spring/dto/RequestResponseDto.java
@@ -6,4 +6,20 @@ package com.ngracia.spring.dto;
 public class RequestResponseDto {
     private String code;
     private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
